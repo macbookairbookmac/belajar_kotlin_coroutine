cb37e05cc7332a3c759d70b13e231822f31a4a84
macbookair <macbookairbookmac@gmail.com>
2022-10-19 09:52:35 +0700

    Configure Memulai-Coroutines.kt

--- src/main/kotlin/Memulai-Coroutines.kt
+++ src/main/kotlin/Memulai-Coroutines.kt
@@ -3,10 +3,10 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-    launch {
+    launch {// (2) launch bekerja
         delay(1000L)
         println("Coroutines!")
     }
-    print("Hello, ")
-    delay(2000L)
+    print("Hello, ") // (1) setelah ini langsung ke launch
+    delay(2000L) // (3) delay bekerja
 }
